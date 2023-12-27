0911a2382f018353ab96384cae9615603ee82f3d
Juan Solar <juansolarsolar@yahoo.com>
2023-12-27 02:47:26 -0300

    correccion coordenadas

--- src/components/WeatherMainInfo.jsx
+++ src/components/WeatherMainInfo.jsx
@@ -19,19 +19,15 @@ export default function WeatherMainInfo({ weather }) {
           </div>
         </div>
         </div>
-        <iframe
-          title="mapa"
-          src={`https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d158858.5851916551!2d${weather?.location.lon}3!3d${weather?.location.lat}!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47d8a00baf21de75%3A0x52963a5addd52a99!2sLondres%2C%20Reino%20Unido!5e0!3m2!1ses-419!2scl!4v1703649628153!5m2!1ses-419!2scl`}
-          width="100%"
-          height="350"
-          style={{ border: 0 }}
-          allowfullscreen=""
-          loading="lazy"
-          referrerPolicy="no-referrer-when-downgrade"
-        >
-
-
-        </iframe>
+      <iframe
+        src={`https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d15057.534307180755!2d${weather.location.lon}5!3d${weather.location.lat}5!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2smx!4v1651103744472!5m2!1sen!2smx`}
+        width="100%"
+        height="350"
+        style={{ border: 0 }}
+        allowfullscreen=""
+        loading="lazy"
+        referrerpolicy="no-referrer-when-downgrade"
+      ></iframe>
     </div>
   )
 }
